8457da7516ecf758b11d17bba52fcab54b97b1ae
CoderRJJ <CoderRJJ@gmail.com>
2021-04-03 12:22:39 +0100

    Updated

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ PlayerSprite = pygame.image.load('assets/Ship.png')
 InvaderAA = pygame.image.load('assets/InvaderA_00.png'); InvaderAB = pygame.image.load('assets/InvaderA_01.png')
 InvaderBA = pygame.image.load('assets/InvaderB_00.png'); InvaderBB = pygame.image.load('assets/InvaderB_01.png')
 InvaderCA = pygame.image.load('assets/InvaderC_00.png'); InvaderCB = pygame.image.load('assets/InvaderC_01.png')
+PlayerBullet = pygame.image.load('assets/PlayerBullet.png');
 
 class Invaders:
     def __init__(self):
@@ -34,9 +35,15 @@ class Invaders:
         ])
 
     def Draw(self, scn, X, Y, Anim):
+        xmin = 9; xmax = 0; ymax = 0
         for y in range(0, 5):
             for x in range(0 , 10):
                 if (self.Population[y,x]):
+                # Find the limits of invaders
+                    if (x<xmin):    xmin=x
+                    elif (x>xmax):  xmax=x
+                    elif (y>ymax):  ymax=y
+                # Draw the invaders
                     if (y==0):
                         if (Anim): DrawInvader = InvaderCA
                         else: DrawInvader = InvaderCB
@@ -48,42 +55,86 @@ class Invaders:
                         else: DrawInvader = InvaderAB
 
                     scn.blit(DrawInvader, (X+(x*30), Y+(y*20)))
+        return(xmin, xmax, ymax)
 
 
 def Main():
-    DisplaySpeed = 100  # Display update speed
+    DisplaySpeed = 20  # Display update speed
     Running = True     # True to keep running else exit
     PlayerSpd = 5      # Player velocity
     PlayerLives = 3    # Player lives
     PlayerX = int(Width/2)
-    InvaderAnim = True
+
+    PlayerBulletSpd = 20    # Player bullet velocity
+    PlayerBulletPosX = 0    # Player bullet X
+    PlayerBulletPosY = -1   # Player bullet Y
+
+    InvaderAnim = 0
+    InvaderFrame=False
+    InvaderFrameRate=20
+
+
+
+    InvaderX=0; InvaderY=50; InvaderSpdX=5; InvaderSpdY=10
 
     MyInvaders=Invaders()
     MyInvaders.Init()
 
     # Loop until the window is closes
     while Running:
-        pygame.display.flip()
-        pygame.time.wait(DisplaySpeed)  # Higher for slower animation
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Running = False
 
+    # Handle Player movement and firing
         keys = pygame.key.get_pressed()
         if (keys[pygame.K_a] and PlayerX - PlayerSpd > 0):
             PlayerX -= PlayerSpd
         if (keys[pygame.K_d] and PlayerX + PlayerSpd < (Width-32)):
             PlayerX += PlayerSpd
+        if (keys[pygame.K_SPACE] and PlayerBulletPosY<0):
+            PlayerBulletPosX = PlayerX + 14
+            PlayerBulletPosY = Height - 28
 
         Screen.fill((BGColor))
         LivesLabel = Font.render("Lives : " + str(PlayerLives), 1, (255, 255, 255))
 
+    # Add lives and score
         Screen.blit(LivesLabel, (10, 10))
+    # Add player
         Screen.blit(PlayerSprite, (PlayerX, Height - 20))
-        MyInvaders.Draw(Screen, 20,50, InvaderAnim)
-        pygame.display.update()
+    # If a Player Bullet is in play update it
+        if (PlayerBulletPosY >= 0):
+            Screen.blit(PlayerBullet, (PlayerBulletPosX, PlayerBulletPosY))
+            PlayerBulletPosY -= PlayerBulletSpd
+    # Update the invaders
+        InvaderRange=MyInvaders.Draw(Screen, InvaderX, InvaderY, InvaderFrame)
+        print (InvaderRange)
 
-        InvaderAnim = not InvaderAnim
+    # Update the space invaders
+        InvaderAnim = (InvaderAnim+1)%InvaderFrameRate
+        InvaderFrame=False
+        if (InvaderAnim < (InvaderFrameRate/2)):InvaderFrame=True
 
+        InvaderXMax=(Width-((InvaderRange[1]+1)*30))
+        InvaderXMin=-((InvaderRange[0])*30)
+        if (InvaderX > InvaderXMax):
+            InvaderY += InvaderSpdY
+            InvaderX = InvaderXMax
+            InvaderSpdX *= -1
+        elif(InvaderX < InvaderXMin):
+            InvaderY += InvaderSpdY
+            InvaderX = InvaderXMin
+            InvaderSpdX *= -1
+        InvaderYMax=(Height-((InvaderRange[2]+1)*20))
+        if (InvaderY >= InvaderYMax):
+            InvaderSpdY=0
+            InvaderSpdX=0
+
+        InvaderX+=InvaderSpdX
+
+    # Finally update the display
+        pygame.display.flip()
+        pygame.time.wait(DisplaySpeed)  # Higher for slower animation
 Main()
